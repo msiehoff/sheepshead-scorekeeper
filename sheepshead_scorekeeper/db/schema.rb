c25aa4ca621763eab911f93f4ba1375950356508
Matt Siehoff <matthew.siehoff@gmail.com>
2017-08-11 22:15:16 -0500

    connect players to game

--- sheepshead_scorekeeper/db/schema.rb
+++ sheepshead_scorekeeper/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812024305) do
+ActiveRecord::Schema.define(version: 20170812031117) do
 
   create_table "games", force: :cascade do |t|
     t.decimal "point_value"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170812024305) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "game_id"
   end
 
 end
